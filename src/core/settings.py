dc9a71c40163cf31ec37ebf89b6fac6e41a65b0a
Adam Harmasz <adam.harmasz@skygate.io>
2021-03-04 15:34:55 +0100

    Add total distance app and basic async requests functions

--- src/core/settings.py
+++ src/core/settings.py
@@ -11,16 +11,18 @@ ALLOWED_HOSTS = ["localhost", "0.0.0.0"]
 
 LOCAL_APPS = []
 
-THIRD_PARTY_APPS = []
+THIRD_PARTY_APPS = ["django_extensions"]
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-] + LOCAL_APPS + THIRD_PARTY_APPS
+]
+
+INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
---
+++ src/total_distance/views.py
@@ -0,0 +1,43 @@
+import asyncio
+
+import httpx
+from asgiref.sync import sync_to_async, async_to_sync
+from django.http import HttpResponse, HttpRequest
+from django.views.decorators.csrf import csrf_exempt
+
+
+@csrf_exempt
+def path_distance(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        request_id = request.POST["request_id"]
+        points = request.POST["points"]
+        get_distance()
+        return HttpResponse(f"{request.POST['request_id']}, {request.POST['points']}")
+
+
+def get_coordinates(points: str):
+    return (dict(longitude=float(longitude), latitude=float(latitude)) for longitude, latitude in points.split(","))
+
+
+@async_to_sync
+async def get_distance():
+    coordinates = (
+        (50.0, 18.1),
+        (51.0, 19.1),
+        (50.5, 15.1),
+        (40.0, 18.1),
+        (49.0, 16.1),
+        (52.0, 18.1)
+    )
+    username = "Cristoforo"
+    password = "Colombo"
+    async with httpx.AsyncClient(auth=httpx.BasicAuth(username=username, password=password)) as session:  # use httpx
+        for idx, coordinate in enumerate(coordinates[:-2]):
+            await asyncio.gather(*[geo_distance_request(session, coordinate, coordinates[idx + 1]) for coordinate in coordinates])
+
+async def geo_distance_request(session, origin, destination):
+    url = "http://64.227.65.68:3006/route"
+    params = dict(origin=f"{origin[0]},{origin[1]}", destination=f"{destination[0]},{destination[1]}")
+    print(params)
+    response = await session.get(url, params=params)
+    print(response)
